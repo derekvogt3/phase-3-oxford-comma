b7c7527b71e4ed5cb8672ab746fd591579fe6e47
Derek Vogt <derekvogt3@gmail.com>
2022-07-19 13:52:41 -0400

    commit tree 4b83f0ddd3433ed7c3ae2b8430b471d4270d857c

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,28 @@
 def oxford_comma(array)
 
+    new_string = ""
+
+    
+
+    array.each_with_index do |item, index|
+
+        if array.length-1 == index 
+            new_string = new_string + item
+
+
+        elsif array.length-2 == index && array.length == 2
+
+            new_string = new_string + item + " and "
+
+        elsif array.length-2 == index 
+            new_string = new_string + item+", and "
+        else 
+            new_string = new_string + item + ", "
+
+        end
+
+    end
+
+    return new_string
+
 end
